dd55f8110b475507d4ece898c993781bca922fa2
Suraj Gupta <surajgupta7070031833@gmail.com>
2025-02-07 13:19:05 +0530

    modified efficient code using slow and fast pointer like linked list

--- Lecture 88(Graph-5)/Reach_Target_Cell_Torus_Grid.cpp
+++ Lecture 88(Graph-5)/Reach_Target_Cell_Torus_Grid.cpp
@@ -20,111 +20,44 @@ OUTPUT : true
 #include <vector>
 using namespace std;
 
-bool can_reach_end(vector<vector<char>>& grid) {
-    int n = grid.size();
-    int m = grid[0].size();
-    vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    char dir_map[] = {'L', 'R', 'U', 'D'};
+// Here we can use dfs, bfs, recursion, or graph but it is not efficient
+// ========= SLow and Fast Pointer (Efficient) ===========
+bool canReachGrid(vector<vector<char>>& grid){
+  /*
+  * Time: O(n*m);
+  * Space: O(1)
+  */  
+  int n = grid.size();
+  int m = grid[0].size();
+  int slow = 0;
+  int fast = 0;
 
-    bool dfs(int i, int j) {
-        if (i < 0 || i >= n || j < 0 || j >= m || grid[i][j] == 'V') {
-            return false;
-        }
-        if (i == n - 1 && j == m - 1) {
-            return true;
-        }
-        char temp = grid[i][j];
-        grid[i][j] = 'V';
-        for (int k = 0; k < 4; k++) {
-            if (dir_map[k] == temp) {
-                if (dfs(i + directions[k].first, j + directions[k].second)) {
-                    return true;
-                }
-            }
-        }
-        grid[i][j] = temp;
-        return false;
+  while(fast < n * m){
+    int next = fast;
+    while(slow <= next){
+      int i = slow / m;
+      int j = slow % m;
+      if(i == n-1 and j == m-1) return true; // Check if we've reached the bottom-right cell
+      if(grid[i][j] == 'L') next = max(next, slow-1);
+      else if(grid[i][j] == 'R') next = min(next, slow+1);
+      else if(grid[i][j] == 'U') next = max(next, slow-m);
+      else if(grid[i][j] == 'D') next = min(next, slow+m);
+      slow++;
     }
-
-    return dfs(0, 0);
+    fast = next+1;
+  }
+  return false;
 }
 
-int main() {
-    vector<vector<char>> grid = {
-        {'R', 'R', 'R'},
-        {'U', 'L', 'R'},
-        {'D', 'D', 'R'}
-    };
-    cout << (can_reach_end(grid) ? "True" : "False") << endl;
-    return 0;
-}
+int main(){
+  
+  vector<vector<char>> grid = {
+    {'R', 'R', 'R'},
+    {'U', 'L', 'R'},
+    {'D', 'D', 'R'}
+  };
 
+  cout<<boolalpha<<canReachGrid(grid)<<endl;
 
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-bool can_reach_end(vector<vector<char>>& grid) {
-    int n = grid.size();
-    int m = grid[0].size();
-    int slow_i = 0, slow_j = 0;
-    int fast_i = 0, fast_j = 0;
-    while (true) {
-        // Move slow pointer
-        if (grid[slow_i][slow_j] == 'L') {
-            slow_j--;
-        } else if (grid[slow_i][slow_j] == 'R') {
-            slow_j++;
-        } else if (grid[slow_i][slow_j] == 'U') {
-            slow_i--;
-        } else if (grid[slow_i][slow_j] == 'D') {
-            slow_i++;
-        }
-        if (slow_i < 0 || slow_i >= n || slow_j < 0 || slow_j >= m) {
-            return false;
-        }
-        if (slow_i == n - 1 && slow_j == m - 1) {
-            return true;
-        }
-        // Move fast pointer
-        int next_i = slow_i, next_j = slow_j;
-        if (grid[next_i][next_j] == 'L') {
-            next_j--;
-        } else if (grid[next_i][next_j] == 'R') {
-            next_j++;
-        } else if (grid[next_i][next_j] == 'U') {
-            next_i--;
-        } else if (grid[next_i][next_j] == 'D') {
-            next_i++;
-        }
-        if (next_i < 0 || next_i >= n || next_j < 0 || next_j >= m) {
-            return false;
-        }
-        if (next_i == n - 1 && next_j == m - 1) {
-            return true;
-        }
-        if (grid[next_i][next_j] == 'L') {
-            fast_j = next_j - 1;
-        } else if (grid[next_i][next_j] == 'R') {
-            fast_j = next_j + 1;
-        } else if (grid[next_i][next_j] == 'U') {
-            fast_i = next_i - 1;
-        } else if (grid[next_i][next_j] == 'D') {
-            fast_i = next_i + 1;
-        }
-        if (fast_i == slow_i && fast_j == slow_j) {
-            return false;
-        }
-    }
-}
-
-int main() {
-    vector<vector<char>> grid = {
-        {'R', 'R', 'R'},
-        {'U', 'L', 'R'},
-        {'D', 'D', 'R'}
-    };
-    cout << (can_reach_end(grid) ? "True" : "False") << endl;
-    return 0;
+  return 0;
 }
