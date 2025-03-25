ad622e5776c0f764a72aed18a745042bac79300a
Suraj Gupta <surajgupta7070031833@gmail.com>
2025-03-25 13:13:57 +0530

    Improving and adding one more solutions

--- Lecture 77(Greedy-3)/Merge_Intervals_START.cpp
+++ Lecture 77(Greedy-3)/Merge_Intervals_START.cpp
@@ -45,6 +45,64 @@ vector<vector<int>>merge(vector<vector<int>>& interval){
     return ans;
 }
 
+/*
+// Define the lambda function as a standalone function bool
+bool cmp(vector<int>& a, vector<int>& b) {
+    return a[0] < b[0];
+}
+
+// Alternatively, you can define the lambda function as a static function
+// static bool cmp(vector<int>& a, vector<int>& b){
+//     return a[0] < b[0];
+// }
+
+// Or, you can define the lambda function inside the merge function
+vector<vector<int>> merge(vector<vector<int>>& interval) {
+    auto cmp = [](vector<int>& a, vector<int>& b) { return a[0] < b[0]; };
+    sort(interval.begin(), interval.end(), cmp);
+    vector<vector<int>> ans;
+    int n = interval.size();
+    ans.push_back(interval[0]); // Start with the first interval
+    for (int i = 1; i < n; i++) {
+        vector<int> currInterval = interval[i];
+        if (currInterval[0] <= ans[ans.size() - 1][1]) {
+            // curr.start <= last.end -> overlapping
+            ans[ans.size() - 1][1] = max(ans[ans.size() - 1][1], currInterval[1]);
+        }
+        else {
+            ans.push_back(interval[i]);
+        }
+    }
+    return ans;
+}
+*/ 
+
+/*
+vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    sort(begin(intervals), end(intervals)); //on the basis of start point
+    
+    vector<vector<int>> result;
+
+    result.push_back(intervals[0]);
+
+    for(int i = 1; i<intervals.size(); i++) {
+
+        int curr_start = intervals[i][0];
+        int curr_end = intervals[i][1];
+
+        //if no overlap
+        if(curr_start > result.back()[1]) {
+            result.push_back(intervals[i]);
+        } else {
+            //there is an overlap
+            result.back()[1] = max(result.back()[1], curr_end);
+        }
+
+    }
+    return result;
+}
+*/ 
+
 int main(){
 
     vector<vector<int>> intervals = {{1,3},{2,6},{8,10},{15,18}};
