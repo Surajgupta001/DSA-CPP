30b36f879cbcc1596b50e6b9dc0f2c3b795f43c3
Suraj Gupta <surajgupta7070031833@gmail.com>
2025-06-17 22:27:33 +0530

    Refactor next greater/smaller element functions and improve output messages

--- Lecture 55(Stack-2)/next_greater_smaller_element.cpp
+++ Lecture 55(Stack-2)/next_greater_smaller_element.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-using namespace std;
 #include <stack>
 #include <vector>
+using namespace std;
 
-// function to finding next greater element
-vector<int>NGE(vector<int> &v){
+// function to finding next greater element to the right
+vector<int>NGER(vector<int> &v){
     
     vector<int> output(v.size(),-1);
 
@@ -22,18 +22,13 @@ vector<int>NGE(vector<int> &v){
     return output;
 }
 
-// function to finding next smaller element
-vector<int>NSE(vector<int> &v){
-    
-    vector<int> output(v.size(),-1);
-
+// function to finding next smaller element to the right
+vector<int> NSER(vector<int> &v) {
+    vector<int> output(v.size(), -1);
     stack<int> st; // indexes
 
-    st.push(0);
-
-    for(int i=1;i<v.size();i++){
-        
-        while(!st.empty() && v[i] < v[st.top()]){ // next greater element.
+    for (int i = 0; i < v.size(); i++) {
+        while (!st.empty() && v[i] < v[st.top()]) {
             output[st.top()] = v[i];
             st.pop();
         }
@@ -41,6 +36,40 @@ vector<int>NSE(vector<int> &v){
     }
     return output;
 }
+
+// function to finding next greater element to the left
+vector<int> NGEL(vector<int> &v) {
+    vector<int> output(v.size(), -1);
+    stack<int> st; // indexes
+
+    st.push(v.size() - 1);
+    for (int i = v.size() - 2; i >= 0; i--) {
+        while (!st.empty() && v[i] > v[st.top()]) {
+            output[st.top()] = v[i];
+            st.pop();
+        }
+        st.push(i);
+    }
+    return output;
+}
+
+// function to finding next smaller element to the left
+vector<int> NSEL(vector<int> &v) {
+    vector<int> output(v.size(), -1);
+    stack<int> st; // indexes
+
+    st.push(v.size() - 1);
+    for (int i = v.size() - 2; i >= 0; i--) {
+        while (!st.empty() && v[i] < v[st.top()]) {
+            output[st.top()] = v[i];
+            st.pop();
+        }
+        st.push(i);
+    }
+    return output;
+}
+
+
 int main(){
 
     int n;
@@ -52,21 +81,37 @@ int main(){
         cin>>v[i];
     }
     
-    // function calling next greater element
-    vector<int>result_nge = NGE(v);
-    cout<<"Next Greater Element: ";
-    for(int i=0;i<result_nge.size();i++){
-        cout<<result_nge[i]<<" ";
+    // function calling next greater element to the right
+    vector<int>result_nger = NGER(v);
+    cout<<"Next Greater Element to Right: ";
+    for(int i=0;i<result_nger.size();i++){
+        cout<<result_nger[i]<<" ";
     }
-
     cout<<endl;
 
-    // function calling next smaller element
-    vector<int>result_nse = NSE(v);
-    cout<<"Next Smaller Element: ";
-    for(int i=0;i<result_nse.size();i++){
-        cout<<result_nse[i]<<" ";
+    // function calling next smaller element to the right
+    vector<int>result_nser = NSER(v);
+    cout<<"Next Smaller Element to Right: ";
+    for(int i=0;i<result_nser.size();i++){
+        cout<<result_nser[i]<<" ";
     }
+    cout<<endl;
+
+    // function calling next greater element to the left
+    vector<int>result_ngel = NGEL(v);
+    cout<<"Next Greater Element to Left: ";
+    for(int i=0;i<result_ngel.size();i++){
+        cout<<result_ngel[i]<<" ";
+    }
+    cout<<endl;
+
+    // function calling next smaller element to the left
+    vector<int>result_nsel = NSEL(v);
+    cout<<"Next Smaller Element to Left: ";
+    for(int i=0;i<result_nsel.size();i++){
+        cout<<result_nsel[i]<<" ";
+    }
+    cout<<endl;
 
     return 0;
 }
