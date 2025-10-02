c5cab6e7bf0b44761336c6ff9a390f4f0ec7e9f0
Suraj Gupta <surajgupta7070031833@gmail.com>
2025-10-02 17:14:55 +0530

    Refactor findCenter function: consolidate approaches, improve readability with comments, and ensure consistent logical operators.

--- Lecture 85(Graph-2)/Find_Center_Star_Graph.cpp
+++ Lecture 85(Graph-2)/Find_Center_Star_Graph.cpp
@@ -21,14 +21,39 @@ Output: 1
 #include <vector>
 using namespace std;
 
-int findCenter(vector<vector<int>>& edges){
-    int a = edges[0][0];
-    int b = edges[0][1];
-    
-    int c = edges[1][0];
-    int d = edges[1][1];
+// Approach-1: Using HashMap
+int findCenter(vector<vector<int>>& edges) {
+    int n = edges.size() + 1; // Number of nodes
+    vector<int> degree(n + 1, 0); // Degree array to count edges for each node
 
-    return (c == a or c == b) ? c : d;
+    // Count the degree of each node
+    for (auto& edge : edges) {
+        degree[edge[0]]++;
+        degree[edge[1]]++;
+    }
+
+    // The center node will have a degree of n - 1
+    for (int i = 1; i <= n; ++i) {
+        if (degree[i] == n - 1) {
+            return i;
+        }
+    }
+
+    return -1; // This line should never be reached for a valid star graph
+}
+
+// Approach-2: Using the property of star graph
+int findCenter(vector<vector<int>>& edges){
+    // The center node must be present in the first two edges
+    int a = edges[0][0]; // First node of the first edge
+    int b = edges[0][1]; // Second node of the first edge
+
+    // Check which node from the first edge is present in the second edge
+    int c = edges[1][0]; // First node of the second edge
+    int d = edges[1][1]; // Second node of the second edge
+
+    // Return the common node
+    return (c == a || c == b) ? c : d;
 }
 
 int main(){
