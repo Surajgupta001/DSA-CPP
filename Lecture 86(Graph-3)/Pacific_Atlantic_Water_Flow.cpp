6c07199650a0f125e5361d6b38b0be6f2f5d360d
Suraj Gupta <surajgupta7070031833@gmail.com>
2025-10-05 23:46:51 +0530

    Multi source BFS DFS

--- Lecture 86(Graph-3)/Pacific_Atlantic_Water_Flow.cpp
+++ Lecture 86(Graph-3)/Pacific_Atlantic_Water_Flow.cpp
@@ -47,25 +47,29 @@ using namespace std;
 // Defining the direction of BFS
 vector<vector<int>> dir = {{1,0}, {-1,0}, {0,1}, {0,-1}}; // i, j -> i+1, j -> i-1, j -> i, j-1 -> i, j+1
 
-int rows;
-int cols;
-vector<vector<int>>h;
-
-vector<vector<bool>> bfs(queue<pair<int, int>>& q){
+vector<vector<bool>> bfs(queue<pair<int, int>>& qu, vector<vector<int>>& h, int rows, int cols){
     vector<vector<bool>> visited(rows, vector<bool>(cols, false));
-    while(not q.empty()){
-        auto cell = q.front();
-        q.pop();
+    while(not qu.empty()){
+        auto cell = qu.front();
+        qu.pop();
+        
         int i = cell.first;
         int j = cell.second;
+        
         visited[i][j] = true;
+        
         for(int d=0; d<4; d++){
+            
             int newRow = i + dir[d][0];
             int newCol = j + dir[d][1];
+            
             if(newRow < 0 or newCol < 0 or newRow >= rows or newCol >= cols) continue; // You exited the grid
+            
             if(visited[newRow][newCol]) continue; // You already visited this cell
+            
             if(h[newRow][newCol] < h[i][j]) continue; // h[newRow][newCol] -> neighbours, h[i][j] -> currentCell
-            q.push({newRow, newCol});
+            
+            qu.push({newRow, newCol});
         }
     }
     return visited;
@@ -74,27 +78,26 @@ vector<vector<bool>> bfs(queue<pair<int, int>>& q){
 // Function to find all cells that can flow to both the Pacific and Atlantic oceans
 vector<vector<int>> pacificAtlantic(vector<vector<int>>& height){
     
-    rows = height.size();
-    cols = height[0].size();
-    h = height;
+    int rows = height.size();
+    int cols = height[0].size();
     
     // Initialize queues for Pacific and Atlantic BFS
     queue<pair<int, int>> pacificBFS;
     queue<pair<int, int>> atlanticBFS;
     
-    // Steps of multisource bfs
+    // Steps of Multi-source BFS
     for(int i=0;i<rows;i++){
         pacificBFS.push({i, 0});
         atlanticBFS.push({i, cols-1});
     }
-    
-    // Add all cells on the Pacific border to the Pacific BFS queue
+
+    // Add all cells on the Pacific border to the Pacific BFS queue & also add all cells on the Atlantic border to the Atlantic BFS queue
     for(int j=1;j<cols;j++) pacificBFS.push({0, j});
     for(int j=0;j<cols;j++) atlanticBFS.push({rows-1, j});
 
     // Perform BFS from both queues
-    vector<vector<bool>>pacific = bfs(pacificBFS);
-    vector<vector<bool>>atlantic = bfs(atlanticBFS);
+    vector<vector<bool>>pacific = bfs(pacificBFS, height, rows, cols);
+    vector<vector<bool>>atlantic = bfs(atlanticBFS, height, rows, cols);
 
     // Find all cells that can flow to both oceans
     vector<vector<int>> ans;
