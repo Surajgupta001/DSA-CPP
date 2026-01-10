fd11d8a114fa47b0af6c2712156f10e159539d3f
Suraj Gupta <surajgupta7070031833@gmail.com>
2026-01-10 12:48:06 +0530

    Refactor LCA algorithm for improved readability and consistency

--- Lecture 67(Binary Tree - 1)/LCA_of_a_Binary_tree.cpp
+++ Lecture 67(Binary Tree - 1)/LCA_of_a_Binary_tree.cpp
@@ -3,7 +3,6 @@ Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in
 
 According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
 
-
 Example 1:
 
 Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
@@ -33,18 +32,27 @@ class node{
 
     // constructor
     node(int data){
-        value = data;
-        left = nullptr;
-        right = nullptr;
+        this->value = data;
+        this->left = nullptr;
+        this->right = nullptr;
     }
 };
 
-bool existInTree(node *root, node *target){
+bool existInTree(node *root, node *targetNode){
     if(root == nullptr) return false;
-    if(root == target) return true;
-    return existInTree(root->left, target) || existInTree(root->right, target);
+    if(root == targetNode) return true;
+    return existInTree(root->left, targetNode) or existInTree(root->right, targetNode);
 }
 
+node *lowestCommonAncestor(node *root, node *p, node *q){
+    if(root == p or root == q) return root;
+    else if(existInTree(root->left, p) and existInTree(root->right, q)) return root;
+    else if(existInTree(root->right, p) and existInTree(root->left, q)) return root;
+    else if(existInTree(root->left, p) and existInTree(root->left, q)) return lowestCommonAncestor(root->left, p, q);
+    else return lowestCommonAncestor(root->right, p, q);
+}
+
+// Modified code of above code for better readability => both code is same
 node *lowestCommonAncestor(node *root, node *p, node *q){
     if(existInTree(root->left, p) && existInTree(root->left, q)) return lowestCommonAncestor(root->left, p, q);
     else if(existInTree(root->right, p) && existInTree(root->right, q)) return lowestCommonAncestor(root->right, p, q);
